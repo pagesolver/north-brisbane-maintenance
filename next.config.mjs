22d30a27bbbdb182efc3f2827c893658663f575e
Zeke Pari <pariezekiel@gmail.com>
2025-11-19 19:17:57 +1000

    fix: update hostname in next.config.mjs for image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "vmbknpp2dlaevhxn.public.blob.vercel-storage.com",
+        hostname: "nojpnistalabissmttgw.supabase.co",
         port: "",
         pathname: "/**",
       },
